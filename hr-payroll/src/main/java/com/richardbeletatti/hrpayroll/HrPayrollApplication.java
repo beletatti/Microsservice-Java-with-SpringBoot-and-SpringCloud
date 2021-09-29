2d3b1f1f87a425070f4c16b789ac4373f77f1aec
Richard Beletatti <richard.beletatti@gmail.com>
2021-09-29 10:27:25 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/richardbeletatti/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/richardbeletatti/hrpayroll/HrPayrollApplication.java
@@ -3,7 +3,9 @@ package com.richardbeletatti.hrpayroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
